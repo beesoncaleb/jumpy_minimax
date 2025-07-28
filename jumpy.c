11011e130f6e4aeac28086d51d96e37584cf1051
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-07-28 02:18:16 -0500

    Initial commit

---
+++ jumpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define BOARD_LEN 4
+
+int static_estimator(int[]);
+
+// Computer move
+int comp_move[4] = [-1, -1, -1, -1 ];
+
+int minimax(int board[], int alpha, int beta, int depth, int max_depth) {
+  if (depth == max_depth) {
+       
+  };
+};
+
+
