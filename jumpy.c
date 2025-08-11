bf28ff602f72076cab13f09de0f6b1557aa0bb0b
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-08-10 19:10:06 -0500

    First version done
    Needs more error handling and optimization, but it's in a usable state. In the future I think I'll remove the static_estimator function and calculate all possible value based on it and use it as a hash function

--- jumpy.c
+++ jumpy.c
@@ -13,6 +13,15 @@ int static_counter = 0;
 int computer_board[BOARD_LEN];
 
 // ASCII art
+char jumpy[] =
+  R"(    _____                                      )" "\n"
+  R"(   |_   _|                                     )" "\n"
+  R"(     | | __   _   _ .--..--.  _ .--.   _   __  )" "\n"
+  R"( _   | |[  | | | [ `.-. .-. |[ '/'`\ \[ \ [  ] )" "\n"
+  R"(| |__' | | \_/ |, | | | | | | | \__/ | \ '/ /  )" "\n"
+  R"(`.____.' '.__.'_/[___||__||__]| ;.__/[\_:  /   )" "\n"
+  R"(                             [__|     \__.'    )" "\n";
+
 char game_start[] =
     R"(   ______       _       ____    ____  ________     ______   _________     _       _______   _________  )" "\n"
     R"( .' ___  |     / \     |_   \  /   _||_   __  |  .' ____ \ |  _   _  |   / \     |_   __ \ |  _   _  | )" "\n"
@@ -25,14 +34,15 @@ char game_start[] =
 int static_estimator(int[], int);
 int compute_win(int[]);
 int minimax(int[], int, int);
-int white_maxi(int[], int, int, int, int);
-int black_mini(int[], int, int, int, int);
+int white_maxi(int[], int, int, int);
+int black_mini(int[], int, int, int);
 void compute_moves(int[], int, int[][BOARD_LEN]);
 void single_move_helper(int, int, int, int, int, int[]);
 void print_board(int*);
-int handle_playerturn(int*);
+void handle_playerturn(int*);
 
 int minimax(int* board, int depth, int player_turn) {
+  static_counter = 0;
   if (depth == 0) {
     return static_estimator(board, player_turn);
   }
@@ -42,30 +52,99 @@ int minimax(int* board, int depth, int player_turn) {
     return wincheck;
   }
 
-  int alpha = NEG_INF;
-  int beta = POS_INF;
   if (player_turn == WHITE_TURN) {
     int computed_moves[2][BOARD_LEN];
     compute_moves(board, player_turn, computed_moves);
+    int value = NEG_INF;
     for (int i=0; i<2; i++) {
       if (computed_moves[i][0] == -1) { continue; }
-
       
+      int curr = black_mini(computed_moves[i], NEG_INF, POS_INF, depth-1);
+      if (curr > value) {
+        value = curr;
+        computer_board[0] = computed_moves[i][0];
+        computer_board[1] = computed_moves[i][1];
+        computer_board[2] = computed_moves[i][2];
+        computer_board[3] = computed_moves[i][3];
+      }
     }
+    return value;
   }
   else {
+    int computed_moves[2][BOARD_LEN];
+    compute_moves(board, player_turn, computed_moves);
+    int value = POS_INF;
+    for (int i=0; i<2; i++) {
+      if (computed_moves[i][0] == -1) { continue; }
+      
+      int curr = white_maxi(computed_moves[i], NEG_INF, POS_INF, depth-1);
+      if (curr < value) {
+        value = curr;
+        computer_board[0] = computed_moves[i][0];
+        computer_board[1] = computed_moves[i][1];
+        computer_board[2] = computed_moves[i][2];
+        computer_board[3] = computed_moves[i][3];
+      }
+    }
+    return value;
   }
 }
 
-int white_maxi(int* board, int alpha, int beta, int depth, int player_turn) {
-  return 0;
+int white_maxi(int board[], int alpha, int beta, int depth) {
+  if (depth == 0) {
+    return static_estimator(board, WHITE_TURN);
+  }
+
+  int wincheck = compute_win(board);
+  if (wincheck != 0){
+    return wincheck;
+  }
+
+  int computed_moves[2][BOARD_LEN];
+  compute_moves(board, WHITE_TURN, computed_moves);
+  int value = NEG_INF;
+  for (int i=0; i<2; i++) {
+    if (computed_moves[i][0] == -1) { continue; }
+
+    int curr = black_mini(computed_moves[i], alpha, beta, depth-1);
+    value = curr > value ? curr : value;
+
+    if (value >= beta) {
+      break;
+    }
+    alpha = value > alpha ? value : alpha;
+  }
+  return value;
 }
 
-int black_mini(int* board, int alpha, int beta, int depth, int player_turn) {
-  return 0;
+int black_mini(int board[], int alpha, int beta, int depth) {
+  if (depth == 0) {
+    return static_estimator(board, BLACK_TURN);
+  }
+
+  int wincheck = compute_win(board);
+  if (wincheck != 0){
+    return wincheck;
+  }
+
+  int computed_moves[2][BOARD_LEN];
+  compute_moves(board, BLACK_TURN, computed_moves);
+  int value = POS_INF;
+  for (int i=0; i<2; i++) {
+    if (computed_moves[i][0] == -1) { continue; }
+
+    int curr = white_maxi(computed_moves[i], alpha, beta, depth-1);
+    value = curr < value ? curr : value;
+
+    if (value <= alpha) {
+      break;
+    }
+    beta = value < beta ? value : beta;
+  }
+  return value;
 }
 
-// helper that can caculate a single move for a1 regardless of color
+// helper that can caculate a single move for a1 regardless of turn
 // use player_turn as direction increment (+1 for white, -1 for black)
 inline void single_move_helper(int a1, int a2, int e1, int e2, int player_turn, int ret_board[]) {
   int pos = a1 += player_turn;
@@ -144,10 +223,10 @@ void compute_moves(int board[], int player_turn, int ret_boards[][BOARD_LEN]) {
     if (b2 != 0) {
       int tmp[BOARD_LEN];
       single_move_helper(b2, b1, w1, w2, player_turn, tmp);
-      ret_boards[1][0] = tmp[3];
-      ret_boards[1][1] = tmp[2];
-      ret_boards[1][2] = tmp[0];
-      ret_boards[1][3] = tmp[1];
+      ret_boards[1][0] = tmp[2];
+      ret_boards[1][1] = tmp[3];
+      ret_boards[1][2] = tmp[1];
+      ret_boards[1][3] = tmp[0];
     }
   }
 }
@@ -236,22 +315,21 @@ void print_board(int* board) {
   int b1 = board[2];
   int b2 = board[3];
 
-  printf("\nCurrent Board\n");
-  printf(" |1|2|3|4|5|6|7|8| \n");
-  char pieces[] = "                   ";   
+  printf("| |1|2|3|4|5|6|7|8| |\n");
 
-  pieces[w1*2] = 'w';
-  pieces[w2*2] = 'w';
-  pieces[b1*2] = 'b';
-  pieces[b2*2] = 'b';
+  char pieces[] = "                   ";   
+  pieces[w1*2+1] = 'w';
+  pieces[w2*2+1] = 'w';
+  pieces[b1*2+1] = 'b';
+  pieces[b2*2+1] = 'b';
   printf("%s\n", pieces);
 }
 
 // come back and add error handling
-inline int handle_playerturn(int* board_ret) {
-  printf("\nPLAYER_MOVE");
+inline void handle_playerturn(int* board_ret) {
+  printf("\n\033[34mPLAYER MOVE\033[0m\n");
   char board_str[6];
-  printf("\nPlayer enter a board position: ");
+  printf("Player enter a board position: ");
   fgets(board_str, 6, stdin);
 
   board_ret[0] = board_str[0]-48;
@@ -259,12 +337,11 @@ inline int handle_playerturn(int* board_ret) {
   board_ret[2] = board_str[2]-48;
   board_ret[3] = board_str[3]-48;
   print_board(board_ret);
-  return 0;
 }
 
 int main() {
   printf("\033[2J\033[H");
-  printf("Welcome to Jumpy\n\n");
+  printf("%s\n\n", jumpy);
   printf("Which side will you play as? (W/B) ");
   while (true) {
     // get player turn
@@ -289,44 +366,50 @@ int main() {
     printf("\033[2J\033[H");
     printf("%s", game_start);
     int board[] = {1, 2, 7, 8};
+    printf("\n\nStarting Board\n");
     print_board(board);
+    printf("-----------------------------------------------------------\n");
     if (player_turn == WHITE_TURN) {
       while (true) {
         handle_playerturn(board);
 
-        // COMPUTER MOVE
-        printf("\nCOMPUTER MOVE\n\n");
-        int comp_boards[2][BOARD_LEN];
-        compute_moves(board, BLACK_TURN, comp_boards);
-        if (comp_boards[0][0] != -1) {
-          printf("Move 1:");
-          print_board(comp_boards[0]);
+        //comp move
+        printf("\n\033[31mCOMPUTER MOVE\033[0m\n");
+        int value = minimax(board, 60, BLACK_TURN);
+        for (int i=0; i<BOARD_LEN; i++) { board[i] = computer_board[i]; } // computer_board is global and stores current comp move
+        print_board(board);
+        printf("\nEstimated Value: %d\n", value);
+        printf("Static estimates: %d\n", static_counter);
+        printf("-----------------------------------------------------------\n");
+
+        if (value == -100) {
+          printf("\nCOMPUTER WINS");
+          break;
         }
-        if (comp_boards[1][0] != -1) {
-          printf("Move 2:");
-          print_board(comp_boards[1]);
-        }
-        break;
       }
     }
     else {
-      // FOR TESTING, REORDER AFTER DONE
       while (true) {
-        handle_playerturn(board);
+        //comp move
+        printf("\n\033[31mCOMPUTER MOVE\033[0m\n");
+        int value = minimax(board, 60, WHITE_TURN);
+        for (int i=0; i<BOARD_LEN; i++) { board[i] = computer_board[i]; } // computer_board is global and stores current comp move
+        print_board(board);
+        printf("\nEstimated Value: %d\n", value);
+        printf("Static estimates: %d\n", static_counter);
+        if (value == 100) {
+          printf("-----------------------------------------------------------\n");
+          printf("\nCOMPUTER WINS");
+          break;
+        }
 
-        // COMPUTER MOVE
-        printf("\nCOMPUTER MOVE\n\n");
-        int comp_boards[2][BOARD_LEN];
-        compute_moves(board, WHITE_TURN, comp_boards);
-        if (comp_boards[0][0] != -1) {
-          printf("Move 1:");
-          print_board(comp_boards[0]);
+        handle_playerturn(board);
+        printf("-----------------------------------------------------------\n");
+
+        if (value == -100) {
+          printf("\nCOMPUTER WINS");
+          break;
         }
-        if (comp_boards[1][0] != -1) {
-          printf("Move 2:");
-          print_board(comp_boards[1]);
-        }
-        break;
       }
     }
     break;
